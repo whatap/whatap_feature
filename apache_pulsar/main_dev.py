84795a03929d3cdbd5d7ae208e74696ea9dcf20a
Hyeongseok Nam <hsnam@whatap.io>
2024-09-25 18:16:43 +0900

    added vcenter

--- apache_pulsar/main_dev.py
+++ apache_pulsar/main_dev.py
@@ -21,11 +21,12 @@ def read_file(file_path):
 def getMetaJson():
     metadata = {
         "desc": {
-            "icon": "pulsar",
+            "icon": "apache-pulsar",
             "pricingUrl": "https://www.whatap.io/ko/pricing/#server-space",
             "summary": {
                 "ko": "Apache Pulsar 모니터링 제품은 Pulsar 클러스터의 성능과 상태를 실시간으로 추적할 수 있게 해줍니다. 메트릭, 로그, 트레이스를 수집하고 시각화하여 문제를 빠르게 식별하고 대응할 수 있습니다. 주요 메트릭에는 메시지 게시 및 소비 속도, 구독 백로그, 스토리지 사용량 등이 포함됩니다. 와탭 Apache Pulsar 모니터링을 통해 효율적인 모니터링과 경고 설정이 가능합니다.",
-                "en": "Apache Pulsar monitoring products enable real-time tracking of the performance and health of Pulsar clusters. They collect and visualize metrics, logs, and traces to quickly identify and respond to issues. Key metrics include message publish and consume rates, subscription backlogs, and storage usage. Efficient monitoring and alerting can be achieved through integrated tools like Whatap Apache Pulsar Monitoring."
+                "en": "Apache Pulsar is an open source messaging and streaming platform that supports high performance, scalability, and multi-tenancy. The Pulsar agents trace the cluster status, metrics for each messaging and node, major topics, and backlogs in real time. By monitoring the key resource usage figures, they allow you to prevent resource overload and errors, and manage message rates and backlogs to detect performance bottlenecks early as possible. By monitoring the key resource usage figures, they allow you to prevent resource overload and errors, and manage message rates and backlogs to detect performance bottlenecks early as possible. You can optimize the Pulsar environment and keep the data flow smooth.",
+                "ja": "Apache Pulsarは、高性能、拡張可能、マルチテナントをサポートするオープンソースメッセージングおよびストリーミングプラットフォームです。 Pulsarエージェントは、クラスターの状況、メッセージングおよびノード別のメトリクス、主なトピックおよびバックログをリアルタイムで追跡します。 主ななリソースの使用量をモニタリングしてリソースの過負荷とエラーを防止でき、メッセージの速度とバックログを管理してパフォーマンスのボトルネックを早期に発見します。 様々な通知設定により、システムに異常が発生した場合、直ちに措置を取れます。Pulsar環境を最適化し、データの流れを円滑に維持してください。",
             }
         },
         "releaseEnvs": ["DEV", "PREVIEW", "SERVICE"]
@@ -39,7 +40,7 @@ def writeMeta():
         f.write(getMetaJson())
     body = {
     "platform":"INFRA",
-    "textKey":"APACHEPULSAR_DEV_ONE",
+    "textKey":"APACHEPULSAR_RC_ONE",
     "name":"ApachePulsar",
     "description":"ApachePulsar BETA RC 0806",
     "status":"beta"}
